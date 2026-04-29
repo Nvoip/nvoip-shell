dc623c86e9b66739da4e46d8aa049fc39748b30c
Leandro Campos <40430440+leandronvoip@users.noreply.github.com>
2026-04-29 17:10:11 -0300

    feat: add shell balance example

---
+++ examples/get-balance.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -eu
+
+SCRIPT_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
+. "$SCRIPT_DIR/../lib/nvoip.sh"
+
+ACCESS_TOKEN="${NVOIP_ACCESS_TOKEN:-$(nvoip_create_access_token)}"
+nvoip_get_balance "$ACCESS_TOKEN"
