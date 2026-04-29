41ec39c333c3a09817151b13f043974380b098e2
Leandro Campos <40430440+leandronvoip@users.noreply.github.com>
2026-04-29 17:10:22 -0300

    feat: add shell call example

---
+++ examples/create-call.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -eu
+
+SCRIPT_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
+. "$SCRIPT_DIR/../lib/nvoip.sh"
+
+nvoip_require_var NVOIP_CALLER
+ACCESS_TOKEN="${NVOIP_ACCESS_TOKEN:-$(nvoip_create_access_token)}"
+TARGET_NUMBER="${NVOIP_TARGET_NUMBER:-11999999999}"
+
+nvoip_create_call "$ACCESS_TOKEN" "$NVOIP_CALLER" "$TARGET_NUMBER"
