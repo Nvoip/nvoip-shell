2b385fe8eea9018f6847e86f9241f603fbfc0283
Leandro Campos <40430440+leandronvoip@users.noreply.github.com>
2026-04-29 16:45:28 -0300

    feat: migrate shell voice torpedo script to API v2

--- Scripts/send_torpedovoz_nvoip.sh
+++ Scripts/send_torpedovoz_nvoip.sh
@@ -1,42 +1,12 @@
-#!/bin/bash
+#!/bin/sh
+set -eu
 
-#Nvoip - Torpedo de Voz Shell Script
+SCRIPT_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
+. "$SCRIPT_DIR/../lib/nvoip.sh"
 
-#Copyright (C) 2020 Nvoip Plataforma Telefonia Ltda
-#Leandro Campos <https://www.linkedin.com/in/leandro-campos/>
-#License https://www.gnu.org/licenses/gpl-3.0.html
+nvoip_require_var NVOIP_CALLER
+ACCESS_TOKEN="${NVOIP_ACCESS_TOKEN:-$(nvoip_create_access_token)}"
+TARGET_NUMBER="${NVOIP_TARGET_NUMBER:-11999999999}"
+AUDIO_MESSAGE="${NVOIP_AUDIO_MESSAGE:-Sua mensagem de voz aqui}"
 
-#This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by #the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-
-#This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
-
-#You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-###Inicio do Script###
-# Seu Token da Nvoip. Acesse https://www.nvoip.com.br, crie sua para ter acesso ao seu Token.
-# English: Your Nvoip Token. Visit https://www.nvoip.com.br, create yours to have access to your Token.
-
-token_auth="TOKEN NVOIP"
-
-# O número do seu usuário(ramal) na Nvoip. O mesmo não precisa estar online.
-# English: Your Nvoip username (extension). It does not have to be online.
-caller="USUÁRIO NVOIP"
-
-#O telefone que irá receber a ligação.
-#English: Phone will receive the voice call.
-called="Telefone/Phone"
-
-#Texto que irá ser lido ou URL de arquivo MP3 que será tocado. A URL deve estar pública na internet e quando acessada via navegador, deverá abrir o player e começar a tocar.
-#English: Text to be read or MP3 file URL to be played. The URL must be public on the internet and when accessed via a browser, should open the player and start playing.
-audio="Texto/MP3"
-
-curl --include \
-     --request POST \
-     --header "Content-Type: application/json" \
-     --header "token_auth: $token_auth" \
-     --data-binary "{
-    \"caller\":\"$caller\",
-    \"called\":\"$called\",
-    \"audio\":\"$audio\"
-}" \
-'https://api.nvoip.com.br/v1/torpedovoz'
+nvoip_send_voice_torpedo "$ACCESS_TOKEN" "$NVOIP_CALLER" "$TARGET_NUMBER" "$AUDIO_MESSAGE"
